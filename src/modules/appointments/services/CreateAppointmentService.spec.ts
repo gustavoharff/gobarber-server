8cd980d458b74f60e9d069db2f0fffd739b983bb
Gustavo Harff <gustavo.harff@gmail.com>
2020-09-18 16:54:17 -0300

    test: create appointment test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,26 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import 'reflect-metadata';
+
+import CreateAppointmentService from './CreateAppointmentService';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '124312412412',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('124312412412');
+  });
+
+  // it('should not be able to create two appointment on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
