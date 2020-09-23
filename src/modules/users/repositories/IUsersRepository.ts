1569d5e643bd95ca509606885127dfbb009d4c47
Gustavo Harff <gustavo.harff@gmail.com>
2020-09-23 16:02:58 -0300

    feat: find all providers router

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,8 +1,9 @@
 import User from '@modules/users/infra/typeorm/entities/User';
-
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProviderDTO from '../dtos/IFindAllProvidersDTO';
 
 export default interface IUsersRepository {
+  findAllProviders(data: IFindAllProviderDTO): Promise<User[]>;
   create(data: ICreateUserDTO): Promise<User>;
   findByEmail(email: string): Promise<User | undefined>;
   findById(id: string): Promise<User | undefined>;
