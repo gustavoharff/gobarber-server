8532e51ecbc70e00973e6daa84f2a4c94354ef11
Gustavo Harff <gustavo.harff@gmail.com>
2020-09-25 17:13:02 -0300

    chore: create appointment

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -19,6 +19,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '321321',
       provider_id: '124312412412',
     });
 
@@ -31,12 +32,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '321321',
       provider_id: '124312412412',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '321321',
         provider_id: '124312412412',
       }),
     ).rejects.toBeInstanceOf(AppError);
