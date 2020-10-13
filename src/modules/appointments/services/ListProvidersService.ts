aeb974d2a016780d153918178cf1fb5b63709af1
Gustavo Harff <gustavo.harff@gmail.com>
2020-10-13 20:03:10 -0300

    fix: class to class

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -20,17 +20,9 @@ class ListProvidersService {
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    let users = await this.cacheProvider.recover<User[]>(
-      `providers-list:${user_id}`,
-    );
-
-    if (!users) {
-      users = await this.usersRepository.findAllProviders({
-        except_user_id: user_id,
-      });
-
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
-    }
+    const users = await this.usersRepository.findAllProviders({
+      except_user_id: user_id,
+    });
 
     return users;
   }
